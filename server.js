8fc8986c88bb0bd2a84698f2d803b539870450aa
kishorec25-ops <kishor.ec25@stellamaryscoe.edu.in>
2026-03-16 20:26:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const Razorpay = require("razorpay");
 const cors = require("cors");
 const crypto = require("crypto");
-const fetch = require("node-fetch");
+
 
 const app = express();
 
