1c01f330a47a417623bb5ff654dbc2015e26b67e
kishorec25-ops <kishor.ec25@stellamaryscoe.edu.in>
2026-03-23 19:12:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 /* ============================= */
 
 const CONFIG = {
-  ESP_IP: "10.187.207.240",   // ✅ YOUR ESP IP
+  ESP_IP: "10.184.181.138",   // ✅ YOUR ESP IP
   WEBHOOK_SECRET: "smartpay123",
 
   AMOUNT_DURATION_MAP: {
